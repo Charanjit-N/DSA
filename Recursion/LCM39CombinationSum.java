deaa40bb08f48e91c5ca2ab70eed894e98c486e2
Charanjit <charanjitnandigama24@gmail.com>
2025-01-21 18:06:50 +0530

    added in recursion

--- Recursion/LCM39CombinationSum.java
+++ Recursion/LCM39CombinationSum.java
@@ -1,24 +1,48 @@
 class LCM39CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-
-        makeCombination(candidates, target, 0, new ArrayList<>(), 0, res);
+        makeCombination(candidates,0,0,target,new ArrayList<>(),res);
+        // 2nd way 
+        // makeCombination(candidates,0,target, new ArrayList<>(),res);
+        
         return res;        
     }
 
-    private void makeCombination(int[] candidates, int target, int idx, List<Integer> comb, int total, List<List<Integer>> res) {
-        if (total == target) {
-            res.add(new ArrayList<>(comb));
+    
+
+    private void makeCombination(int[] candidates, int index, int sum, int target, List<Integer> ls,List<List<Integer>> res) {
+        if(index== candidates.length){
+            if(sum==target){
+                res.add(new ArrayList<>(ls));
+            }
             return;
         }
-
-        if (total > target || idx >= candidates.length) {
-            return;
-        }
-
-        comb.add(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.remove(comb.size() - 1);
-        makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
+       if(sum<target){
+        ls.add(candidates[index]);
+        sum+=candidates[index];
+        makeCombination(candidates,index,sum,target,ls, res);
+        
+        ls.remove(ls.size() - 1);
+        sum-=candidates[index];
+       }
+        makeCombination(candidates,index+1,sum,target,ls, res);
+    }   
+    // 2nd way   
+    // private void makeCombination(int[] candidates, int index, int target, List<Integer> ls,List<List<Integer>> res) {
+    //     if(index== candidates.length){
+    //         if(target ==0){
+    //             res.add(new ArrayList<>(ls));
+    //         }
+    //         return;
+    //     }
+    //    if(candidates[index]<=target){
+    //     ls.add(candidates[index]);
+    //     makeCombination(candidates,index,target-candidates[index],ls, res);
+        
+    //     ls.remove(ls.size() - 1);
+    //    }
+    //     makeCombination(candidates,index+1,target,ls, res);
+    // } 
 }
+
+  
